b7870e850cc68c2360b6eccb3a4fd8c04f091b59
botrf <245.138.3244.3l4@gmail.com>
2021-10-13 18:37:30 +0300

    the last commit for today.

--- project_task/router.py
+++ project_task/router.py
@@ -1,4 +1,4 @@
-from store.viewsets import ProductViewset, ProductDetailViewset
+from store.viewsets import ProductViewset #, ProductDetailViewset
 from rest_framework import routers
 
 
@@ -6,6 +6,6 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('product', ProductViewset)
-router.register('product/<int:pk>/', ProductDetailViewset)
+# router.register('product/<int:pk>/', ProductDetailViewset)
 
 urlpatterns = router.urls
